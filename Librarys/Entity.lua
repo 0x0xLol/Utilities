9f2f96e3ca554037871deefd8a17a89cddf2cdb0
0x0xLol <139097481+0x0xLol@users.noreply.github.com>
2024-07-29 14:52:38 -0400

    Update Entity.lua

--- Librarys/Entity.lua
+++ Librarys/Entity.lua
@@ -1,1 +1,24 @@
+local Entity = {}
 
+local PlayerService = game:GetService("Players")
+
+Entity.GetLocalPlayer = function()
+	return PlayerService.LocalPlayer
+end
+Entity.GetPlayer = function(Name)
+	for _, plrs in PlayerService:GetPlayers() do
+		if plrs.Name == Name then
+			return plrs
+		end
+	end
+	print("Failed to get Player.")
+	return nil
+end
+Entity.GetCharacter = function()
+	return Entity.GetPlayer().Character
+end
+Entity.GetCharacterFromWorkspace = function()
+	return workspace:FindFirstChild(Entity.GetLocalPlayer().Name)
+end
+
+return Entity
