1dee4c056923555f158a495ab73f1e0db9f923d3
0x0xLol <139097481+0x0xLol@users.noreply.github.com>
2024-07-29 14:58:00 -0400

    Update Game.lua

--- Librarys/Game.lua
+++ Librarys/Game.lua
@@ -1,9 +1,13 @@
 local Library = {}
 
 local UserInputService = game:GetService("UserInputService")
+local PlayerService = game:GetService("Players")
 
 Library.IsMoving = function()
 	return UserInputService:IsKeyDown("W") or UserInputService:IsKeyDown("A") or UserInputService:IsKeyDown("S") or UserInputService:IsKeyDown("D") or UserInputService:IsKeyDown("Space")
+end
+Library.onGround = function(Player)
+	return PlayerService[Player].Character.Humanoid.FloorMaterial ~= Enum.Material.Air
 end
 
 return Library
